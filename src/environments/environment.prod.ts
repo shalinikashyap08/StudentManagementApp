ecd870c1aee419a021318403f0bdecc55c9a1723
shalinikashyap08 <shalini.kashyap@chicmic.co.in>
2021-06-24 11:14:12 +0530

    deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAGAdR25eZR_trAf2fG4mGWeb-vk3JATQk",
+    authDomain: "student-app-7e8e9.firebaseapp.com",
+    databaseURL: "https://student-app-7e8e9-default-rtdb.firebaseio.com",
+    projectId: "student-app-7e8e9",
+    storageBucket: "student-app-7e8e9.appspot.com",
+    messagingSenderId: "368531102055",
+    appId: "1:368531102055:web:e7ba9c7c3ade4eb370f7c2",
+    measurementId: "G-2LL5LF12DF"
+  }
 };
